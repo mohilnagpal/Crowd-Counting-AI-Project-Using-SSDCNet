246816c47f8a3cf61db93c846358b2e0440f9803
mohilnagpal <49190511+mohilnagpal@users.noreply.github.com>
2020-11-21 22:25:08 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,139 @@
+import os
+from termcolor import colored
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from google.colab.patches import cv2_imshow
+import argparse
+import cv2
+import numpy as np
+import torch
+
+from Network.SDCNet import SDCNet_VGG16_classify
+from Network.SSDCNet import SSDCNet_classify
+from utils import VideoStream
+
+
+def main(model_path, input_source, SDCNet=False):
+
+    if os.path.isfile(input_source):
+        video_name = input_source.split("/")[-1]
+        video_path = input_source.replace(video_name,"")
+        print("Processing video {} in {}".format(colored(video_name, 'red'), colored(video_path, 'green')))
+        cap = cv2.VideoCapture(input_source)
+    else:
+        cap = VideoStream(input_source).start()
+
+    # --1.2 use initial setting to generate
+    # set label_indice
+    label_indice = np.arange(0.5, 22+0.5/2, 0.5)
+    add = np.array([1e-6, 0.05, 0.10, 0.15, 0.20,
+                    0.25, 0.30, 0.35, 0.40, 0.45])
+    label_indice = np.concatenate((add, label_indice))
+
+    # init networks
+    label_indice = torch.Tensor(label_indice)
+    class_num = len(label_indice)+1
+
+    div_times = 2
+
+    if SDCNet:
+        net = SDCNet_VGG16_classify(
+            class_num, label_indice, load_weights=True).cuda()
+    else:
+        net = SSDCNet_classify(class_num, label_indice, div_times=div_times,
+                               frontend_name='VGG16', block_num=5,
+                               IF_pre_bn=False, IF_freeze_bn=False, load_weights=True,
+                               parse_method='maxp').cuda()
+
+    if os.path.exists(model_path):
+        print("Adding Weights ....")
+        all_state_dict = torch.load(model_path)
+        net.load_state_dict(all_state_dict['net_state_dict'],strict=False)
+        #model.load_state_dict(checkpoint['state_dict'], strict=False)
+        net.eval()
+    else:
+        print("Can't find trained weights!!")
+        exit()
+
+    first_frame = True
+  
+    while True:
+        flag, image = cap.read()
+        output_image = np.copy(image)
+        if first_frame:
+            rois = cv2.selectROIs("frame", image, False, False)
+            first_frame = False
+            # print(roi)
+        if flag:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        else:
+            break
+
+
+        sum = 0
+        for roi in rois:
+            roi_image = image[int(roi[1]):int(roi[1]+roi[3]),
+                              int(roi[0]):int(roi[0]+roi[2])]
+
+            roi_image = cv2.resize(roi_image, (256, 256))
+
+            roi_image = np.transpose(roi_image, (2, 0, 1))
+            roi_image = torch.Tensor(roi_image[None, :, :, :])
+            # w = image.shape[-1]
+            # h = image.shape[-2]
+            # pad_w = 64 - w%64
+            # padding_left = int(pad_w/2)
+            # padding_right = pad_w - padding_left
+            # pad_h = 64 - h%64
+            # padding_top = int(pad_h/2)
+            # padding_bottom = pad_h - padding_top
+            # image = torch.nn.functional.pad(image, (padding_left, padding_right, padding_top, padding_bottom))
+
+            if torch.cuda.is_available():
+                roi_image = roi_image.cuda()
+            with torch.no_grad():
+                features = net(roi_image)
+                div_res = net.resample(features)
+                merge_res = net.parse_merge(div_res)
+                if SDCNet:
+                    outputs = merge_res['div'+str(net.args['div_times'])]
+                else:
+                    outputs = merge_res['div'+str(net.div_times)]
+
+                del merge_res
+
+            cv2.rectangle(output_image, (int(roi[0]), int(roi[1])), (int(
+                roi[0]+roi[2]), int(roi[1]+roi[3])), (255, 0, 0), thickness=3)
+            sum += int(outputs.sum().item())
+
+        cv2.putText(output_image, "{}".format(sum),
+                    (30, 50), cv2.FONT_HERSHEY_PLAIN, 2,
+                    (255, 0, 0), 3)
+
+        cv2.imshow("frame", output_image)
+        if cv2.waitKey(1) == ord('q'):
+            cap.release()
+            exit()
+
+    cap.release()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        "S-DCNet: Spatial Divide-and-Conquer Crowd Counting")
+    parser.add_argument("model", type=str, help="Pretrained weights")
+    parser.add_argument("--video", "-v", type=str,
+                        default=None, help="The video path to crowd count")
+    parser.add_argument("--cap", "-c", type=int,
+                        default=0, help="The video path to crowd count")
+    parser.add_argument("--SDCNet", action='store_true',
+                        default=False, help="Check it if you want to use SDCNet")
+    args = parser.parse_args()
+
+    if args.video:
+        input_source = args.video
+    else:
+        input_source = args.cap
+
+    main(args.model, input_source, SDCNet=args.SDCNet)
---
+++ train.py
@@ -0,0 +1,319 @@
+import sys
+import os
+import os.path
+from sys import exit as e
+from os.path import join as pjn
+import copy
+import glob
+import shutil
+from datetime import datetime
+import math
+import hydra
+import logging
+from tqdm import tqdm, trange
+import q
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from tensorboardX import SummaryWriter
+
+import ShanghaiTech_dataset as dtst
+
+from SDCNet import SDCNet
+from labels_counts_utils import make_label2count_list
+import loss
+
+
+def print_dbg_info_dataloader(loader):
+    """
+    Prints image names and tensor shapes for samples in a DataLoader.
+    Was used for debugging.
+    """
+    for i, sample in enumerate(loader):
+        print(f"i = {i}; image bname = {sample['image_bname']}; "
+              f"image shape = {tuple(sample['image'].shape)}")
+        cs = sample['counts_gt']
+        ls = sample['labels_gt']
+        print(f"counts_gt shapes = {[tuple(l.shape) for l in cs]}; "
+              f"labels_gt shapes = {[tuple(l.shape) for l in ls]}")
+        print()
+
+
+def get_dataloaders(cfg, train_val_test_mask):
+    interval_bounds, label2count_list = make_label2count_list(cfg)
+
+    assert len(label2count_list) == interval_bounds.shape[0] + 1, \
+        "Number of class labels must be equal to (1 + number of interval " \
+        "boundaries), but the equality does not hold"
+
+    rgb_mean_train = dtst.calc_rgb_mean_train(cfg)
+
+    composed_transf_train = transforms.Compose([
+        dtst.Normalize(rgb_mean_train),
+        dtst.ToCHW(),
+        dtst.RandomHorizontalFlip(p=0.5),
+        dtst.QuasiRandomCrop(),
+        dtst.PadToMultipleOf64(),
+        dtst.AddGtCountsLabels(interval_bounds),
+    ])
+
+    composed_transf_test = transforms.Compose([
+        dtst.Normalize(rgb_mean_train),
+        dtst.ToCHW(),
+        dtst.PadToMultipleOf64(),
+    ])
+
+    if train_val_test_mask[0]:
+        train_dataset = dtst.ShanghaiTechDataset(
+            cfg,
+            subdir='train_data',
+            shuffle_seed=42,
+            rel_inds=(0.0, cfg.train.train_val_split),
+            transform=composed_transf_train)
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=cfg.train.batch_size,
+            shuffle=True,
+            num_workers=4)
+    else:
+        train_loader = None
+
+    if train_val_test_mask[1]:
+        val_dataset = dtst.ShanghaiTechDataset(
+            cfg,
+            subdir='train_data',
+            shuffle_seed=42,
+            rel_inds=(cfg.train.train_val_split, 1.0),
+            transform=composed_transf_test)
+        val_loader = DataLoader(
+            train_dataset,
+            batch_size=cfg.validation.batch_size,
+            shuffle=False,
+            num_workers=4)
+    else:
+        val_loader = None
+
+    if train_val_test_mask[2]:
+        test_dataset = dtst.ShanghaiTechDataset(
+            cfg,
+            subdir='test_data',
+            shuffle_seed=None,
+            rel_inds=(0.0, 1.0),
+            transform=composed_transf_test)
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=cfg.test.batch_size,
+            shuffle=False,
+            num_workers=4)
+    else:
+        test_loader = None
+
+    return train_loader, val_loader, test_loader
+
+
+class TrainManager(object):
+    def __init__(
+            self,
+            model,
+            optimizer,
+            cfg,
+            additional_cfg,
+            train_loader,
+            val_loader):
+        self.model = model
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.optimizer = optimizer
+        self.cfg = cfg
+        self.add_cfg = additional_cfg
+        self.tbx_wrtr_dir = additional_cfg.get('tbx_wrtr_dir')
+        self.orig_cwd = hydra.utils.get_original_cwd()
+        self.tbx_wrtr = additional_cfg.get('tbx_wrtr')
+
+    def validate(self, data_loader, step=0):
+        self.model.eval()
+        diffs_count_pred_gt = []
+        mae_sum = 0.0
+        mse_sum = 0.0
+
+        with torch.no_grad():
+            for sample in data_loader:
+                gt_count = sample['dmap'].numpy().sum()
+                image = sample['image'].float().to(self.add_cfg['device'])
+                *cls_logits_list, DIV2, U1, U2, W1, W2 = self.model(image)
+                pred_count = DIV2.cpu().numpy().sum()
+                diff = pred_count - gt_count
+                diffs_count_pred_gt.append(diff)
+                mae_sum += abs(diff)
+                mse_sum += diff**2
+
+        N = len(data_loader)
+        mae = mae_sum / N
+        mse = math.sqrt(mse_sum / N)
+
+        return (mae, mse)
+
+    def train(self):
+        batch_iter = 0
+        epoch = 0
+
+        if self.cfg.validation.if_val_before_begin_train:
+            initial_val_mae, initial_val_mse = self.validate(
+                self.val_loader, step=epoch)
+            initial_tr_mae, initial_tr_mse = self.validate(
+                self.train_loader, step=epoch)
+            self.tbx_wrtr.add_scalars(
+                'error_values/MAE',
+                {'val': initial_val_mae, 'train': initial_tr_mae},
+                epoch)
+            self.tbx_wrtr.add_scalars(
+                'error_values/MSE',
+                {'val': initial_val_mse, 'train': initial_tr_mse},
+                epoch)
+
+        print()
+        print(f"  The checkpoints and tensorboard events files are saved to "
+              f"'{os.path.relpath(self.tbx_wrtr_dir, self.orig_cwd)}'")
+        print("  Progress bar for training epochs:")
+        end_epoch = self.cfg.train.start_epoch + self.cfg.train.num_epochs
+        for epoch in trange(self.cfg.train.start_epoch, end_epoch):
+            self.model.train()
+            self.adjust_learning_rate(epoch)
+
+            avg_lr = 0
+            for param_group in self.optimizer.param_groups:
+                avg_lr += param_group['lr']
+            avg_lr /= len(self.optimizer.param_groups)
+            self.tbx_wrtr.add_scalar(
+                'hyperparams/log10_learning_rate', math.log10(avg_lr), epoch)
+
+
+            for sample in self.train_loader:
+                gt_cls0_label, gt_cls1_label, gt_cls2_label = sample['labels_gt']
+                sample_counts_gt = [torch.unsqueeze(c, 1) for c in sample['counts_gt']]
+                gt_div2 = sample_counts_gt[-1]
+                image = sample['image'].float().to(self.add_cfg['device'])
+                cls0_logits, cls1_logits, cls2_logits, DIV2, U1, U2, W1, W2 \
+                    = self.model(image)
+                self.optimizer.zero_grad()
+
+                cross_entropy_loss_terms = loss.counter_loss(
+                    (gt_cls0_label, gt_cls1_label, gt_cls2_label),
+                    (cls0_logits, cls1_logits, cls2_logits))
+                
+                merging_loss = loss.merging_loss(gt_div2, DIV2)
+                
+                losses_list = [cross_entropy_loss_terms, merging_loss]
+                if self.cfg.model.supervised:
+                    upsampling_loss = loss.upsampling_loss(sample_counts_gt, U1, U2)
+                    losses_list.append(upsampling_loss)
+                    Cmax = self.model.label2count_tensor[-1]
+                    division_loss = loss.division_loss(sample_counts_gt, W1, W2, Cmax)
+                    losses_list.append(division_loss)
+                
+                total_loss = loss.total_loss(losses_list)
+                
+                for i, CE_term in enumerate(cross_entropy_loss_terms):
+                    self.tbx_wrtr.add_scalar(
+                        f'losses/cls_{i}_loss', CE_term, batch_iter)
+                self.tbx_wrtr.add_scalar(
+                    'losses/merging_loss', merging_loss, batch_iter)
+
+                if self.cfg.model.supervised:
+                    self.tbx_wrtr.add_scalar(
+                        'losses/upsampling_loss', upsampling_loss, batch_iter)
+                    self.tbx_wrtr.add_scalar(
+                        'losses/division_loss', division_loss, batch_iter)
+                
+                self.tbx_wrtr.add_scalar(
+                    'losses/total_loss', total_loss, batch_iter)
+
+                total_loss.backward()
+                self.optimizer.step()
+                batch_iter += 1
+
+            if epoch % self.cfg.validation.validate_ckpt_every_epochs == 0:
+                val_mae, val_mse = self.validate(self.val_loader, step=epoch)
+                tr_mae, tr_mse = self.validate(self.train_loader, step=epoch)
+                self.tbx_wrtr.add_scalars(
+                    'error_values/MAE',
+                    {'val': val_mae, 'train': tr_mae},
+                    epoch)
+                self.tbx_wrtr.add_scalars(
+                    'error_values/MSE',
+                    {'val': val_mse, 'train': tr_mse},
+                    epoch)
+            
+            if epoch % self.cfg.validation.save_ckpt_every_epochs == 0:
+                nm = f'epoch_{epoch:04d}.pth'
+                if not os.path.isdir(pjn(self.tbx_wrtr_dir, 'checkpoints')):
+                    os.mkdir(pjn(self.tbx_wrtr_dir, 'checkpoints'))
+                self.save_ckpt(
+                    epoch=epoch,
+                    fpath=pjn(self.tbx_wrtr_dir, 'checkpoints', nm))
+
+    def adjust_learning_rate(self, epoch):
+        lr_section = self.cfg.train.lr_schedule
+        lr = lr_section.lr_init * (lr_section.lr_anneal_rate ** epoch)
+        # update optimizer's learning rate
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def save_ckpt(self, epoch, fpath):
+        d = {
+            'epoch': epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+        }
+        torch.save(d, fpath)
+
+
+@hydra.main(config_path="conf", config_name="config_train_val_test.yaml")
+
+def main(cfg):
+    orig_cwd = hydra.utils.get_original_cwd()
+    train_loader, val_loader, test_loader = get_dataloaders(cfg, (1, 1, 0))
+    interval_bounds, label2count_list = make_label2count_list(cfg)
+    
+    model = SDCNet(
+        label2count_list,
+        cfg.model.supervised,
+        load_pretr_weights_vgg=True)
+
+    if cfg.train.pretrained_ckpt:
+        print(f"  Using pretrained model and its checkpoint "
+              f"'{cfg.train.pretrained_ckpt}'")
+        loaded_struct = torch.load(pjn(orig_cwd, cfg.train.pretrained_ckpt))
+        model.load_state_dict(loaded_struct['model_state_dict'], strict=True)
+    
+    additional_cfg = {'device': None}
+    if not cfg.resources.disable_cuda and torch.cuda.is_available():
+        additional_cfg['device'] = torch.device('cuda')
+        model = model.cuda()
+    else:
+        additional_cfg['device'] = torch.device('cpu')
+
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=cfg.train.lr_schedule.lr_init,
+        momentum=cfg.train.optimizer.momentum,
+        weight_decay=cfg.train.optimizer.weight_decay)
+
+    additional_cfg['tbx_wrtr_dir'] = os.getcwd()
+    with SummaryWriter(additional_cfg['tbx_wrtr_dir']) as tbx_wrtr:
+        additional_cfg['tbx_wrtr'] = tbx_wrtr
+        trainer = TrainManager(
+            model,
+            optimizer,
+            cfg,
+            additional_cfg,
+            train_loader=train_loader,
+            val_loader=val_loader)
+        trainer.train()
+
+
+if __name__ == "__main__":
+    main()
